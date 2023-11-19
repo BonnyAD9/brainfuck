af03f4918983e18376890a9138a3e1ec43f5e84c
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2023-11-19 19:04:08 +0100

    format

--- c/src/interpreter.c
+++ c/src/interpreter.c
@@ -14,7 +14,8 @@ void interpret(const Vec code, Vec *tape) {
 
         ti += i.move;
 
-        VEC_AT(char, *tape, ti) = (VEC_AT(char, *tape, ti) + i.add) % tape->len;
+        VEC_AT(char, *tape, ti) =
+            (VEC_AT(char, *tape, ti) + i.add) % tape->len;
 
         if (i.flags & INST_PRINT) {
             putchar(VEC_AT(char, *tape, ti));
